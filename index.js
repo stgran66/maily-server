268831b13fcc50bbfa655b5d4b853fee6c4458fc
stgran66 <stanislaw.grygoriew@gmail.com>
2024-02-22 16:17:16 +0100

    added passport middleware to deal with google oauth and created user model class with mongoose

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(process.env.MONGO_URI);
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+require('./routes/authRoutes')(app);
 
 app.listen(PORT);
