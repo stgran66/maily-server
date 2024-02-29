780cfff3b54f60a709ad931f61559576081650a5
stgran66 <stanislaw.grygoriew@gmail.com>
2024-02-29 18:55:04 +0100

    updated build path

--- index.js
+++ index.js
@@ -30,8 +30,10 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+console.log(process.env.NODE_ENV);
+
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client'));
+  app.use(express.static('client/build'));
 
   const path = require('path');
   app.get('*', (req, res) => {
